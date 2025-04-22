cf58022c440416a0c0845062442949bfcea58a1e
amitpandey8834 <amitzuespandey@gmail.com>
2025-04-23 04:21:58 +0530

    added frontend to and fixed server.js

--- Todo-Backend/server.js
+++ Todo-Backend/server.js
@@ -1,19 +1,25 @@
-const express = require("express")
- const cors = require("cors")
- const bodyParser = require("body-parser")
- const dotenv = require("dotenv")
- const connectDB = require("./db")
- const todoRoutes = require("./routes/todoRoutes")
- 
- dotenv.config()
- 
- const app = express();
- app.use(cors())
- app.use(bodyParser.json())
- app.use(express.json())
- app.use('/api', todoRoutes)
- 
- connectDB()
- 
- module.exports = app;
- // app.use()
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+const todoRoutes = require("./routes/todoRoutes");
+
+const path = require("path");
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use('/api', todoRoutes);
+
+connectDB();
+
+app.use(express.static(path.join(__dirname, "../todo-frontend/build")));
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../todo-frontend/build", "index.html"));
+});
+
+module.exports = app;
